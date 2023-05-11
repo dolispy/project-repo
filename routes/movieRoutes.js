ec68bc6e1148a3534eeed4e34d272a8c31b9e6a7
Benjamin Afunsho <ben.afunsho@gmail.com>
2023-05-11 07:53:18 +0100

    Delete routes directory

--- routes/movieRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const movieValidation = require('../validate');
-const { addMovie, getMovies } = require('../controllers/movie.controller');
-
-const router = express.Router();
-router.get('/movies', getMovies);
-router.post('/movie/add', movieValidation, addMovie)
-
-module.exports = router;
